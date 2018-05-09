100225a00adc6b851be028049662463952b18769
Avril Dunleavy <dunleavyavril@gmail.com>
2018-05-09 15:03:42 -0700

    change of db

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+# gem 'sqlite3'
 gem 'rails', '~> 5.1.6'
 # Use sqlite3 as the database for Active Record
 gem 'shoulda-matchers', '~> 3.1'
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180509173528) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "visits", force: :cascade do |t|
     t.integer "user_id"
     t.string "name"
